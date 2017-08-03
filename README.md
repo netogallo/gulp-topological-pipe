7533ef491a6c614ba833732e9333ad1fdae93e65
Ernesto Rodriguez <neto@netowork.me>
2017-08-03 10:24:33 -0600

    Converted SortedPackages to a pipe for better usability

--- README.md
+++ README.md
@@ -7,15 +7,18 @@ Stream intended to be used with gulp which is designed to run a command on multi
 Here is a small example shoing how a command would be executed on multiple projects inside a folder called *local_packages*.
 
 ```
+const gulp = require('gulp');
 const topo = require('gulp-topological-pipe');
 const through2 = require('through2');
 
 gulp.task('for-each-package', () => {
-    return topo.Packages.SortedPackages(
+    return gulp.src(
     [
         './local_packages/**/package.json',      // Glob matching all the package.json files in the local_packages directory
         '!./local_packages/**/node_modules/**/*' // Glob to exclude things located in a node_modules directory just in case
-    ]).pipe(through2(
+    ])
+    .pipe(topo.Packages.SortedPackages())
+    .pipe(through2(
         {objectMode: true}, // Must be true because SortedPacakges is an object stream
         (package, enc, callback => {
             // Do stuff with package. See: doc/classes/_package_.package.html to learn about the package fields
--- src/Files.ts
+++ src/Files.ts
@@ -10,37 +10,19 @@ import * as File from 'vinyl';
 
 import {defaultConfig, IConfig} from './Config';
 import {Package} from './Package';
-import {PackageStream} from './PackageStream';
 
 export class Packages{
+    public static CreatePackage(chunck){
+        var file : File = chunck;
+        var json : string;
 
-    public static readonly PackageJson = "package.json";
-
-    public static GetPackages(src : string[])
-    {
-        return gulp.src(src);
-    }
-
-    public static LoadPackages(src : string[])
-    {
-        var packages = Packages.GetPackages(src);
-        var th = through2(
-            {objectMode: true},
-            (chunck, enc, callback) => {
-                var file : File = chunck;
-                var json : string;
-
-                if(<Buffer>file.contents)
-                {
-                    var content : Buffer = <Buffer>file.contents;
-                    json = content.toString('utf-8');
-                }
-                
-                th.push(new Package(JSON.parse(json), file.path));
-                callback();
-            });
-
-        return packages.pipe(th);
+        if(<Buffer>file.contents)
+        {
+            var content : Buffer = <Buffer>file.contents;
+            json = content.toString('utf-8');
+        }
+        
+        return new Package(JSON.parse(json), file.path);
     }
 
     private static RemoveSubPackages(packages : Dictionary<string,Package>){
@@ -51,7 +33,7 @@ export class Packages{
                 keys,
                 key => {
                     var pkg2 = packages.getValue(key);
-                    return pkg2 && key != pkg.Name && ld.includes(dirname(pkg2.Path), dirname(pkg.Path));
+                    return pkg2 && key !== pkg.Name && ld.includes(dirname(pkg2.Path), dirname(pkg.Path));
             })
             .forEach((key, ix, arr) => {
                 packages.remove(key);
@@ -78,25 +60,28 @@ export class Packages{
         return sorted.reverse().map<Package>(value => packages.getValue(value));
     }
 
-    public static SortedPackages(src : string[], config_? : IConfig) : Readable
-    {
+    public static SortedPackages(config_? : IConfig){
         var config = config_ ? config_ : defaultConfig;
-        var packages = Packages.LoadPackages(src);
-        var result = new PackageStream();
         var mappings = new Dictionary<string, Package>();
+        return through2(
+            {objectMode: true},
+            function(chunck, enc, cb){
+                var pkg = Packages.CreatePackage(chunck);
+                mappings.setValue(pkg.Name, pkg);
+                cb();
+            },
+            function(cb){
+                var sorted = Packages.SortPackages(mappings);
+                
+                if(!config.keep_sub_packages){
+                    Packages.RemoveSubPackages(mappings);
+                }
 
-        packages.on('data', (data) => mappings.setValue((<Package>data).Name, data));
-        packages.on('end', () => {
-
-            if(!config.keep_sub_packages){
-                Packages.RemoveSubPackages(mappings);
+                for(let pkg of sorted){
+                    this.push(pkg);
+                }
+                cb();
             }
-
-            var sorted : Package[] = Packages.SortPackages(mappings);
-            sorted.push(null);
-            result.PushPackages(sorted);
-        });
-
-        return result;
+        )
     }
 }
